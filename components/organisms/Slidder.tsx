9ee03b00f2758f1abd0a68aa8d02b62e87d8f07c
Pablo <contactoemail.pablo@gmail.com>
2022-08-31 12:40:52 +0200

    Slide Test 1.2

--- components/organisms/Slidder.tsx
+++ components/organisms/Slidder.tsx
@@ -1,12 +1,16 @@
 import {motion} from "framer-motion";
 const Slidder = () => {
     return(
-        <motion.div className="bg-blue-300 h-[60vh] w-[100vw]"
-            drag="x"
-            dragConstraints={{left: 0, right:300}}
-            dragSnapToOrigin
+        <motion.div className="bg-blue-300 h-[10rem] w-[100vw] flex justify-center items-center text-3xl"
+            drag="y"
+            dragConstraints={{top: 50, bottom:10}}
+            whileDrag={{
+                scale: 0.9,
+                rotateX: 70,
+                
+            }}
         >
-            
+            <h1>Text</h1>    
         </motion.div>
     );
 };
