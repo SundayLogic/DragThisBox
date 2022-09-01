4fe162d47bb6e6d5ff689a8b24745675f9e97b87
Pablo <contactoemail.pablo@gmail.com>
2022-09-01 11:55:42 +0200

    Zustand Test with button

--- components/atoms/Buttons/BtnTest.tsx
+++ components/atoms/Buttons/BtnTest.tsx
@@ -1,2 +1,25 @@
-const BtnTest = () =>{};
+import { useAnimation } from "framer-motion";
+import React, { useRef, useState } from "react";
+import { useButton } from "react-aria";
+
+interface ButtonProps {
+  onClick: () => void;
+  children: string | number;
+}
+
+function Button({ onClick, children }: ButtonProps) {
+    return <button onClick={() => onClick()}>{children}</button>
+}
+
+const BtnTest = () => {
+  const [result, setResult] = useState<number>(0);
+  return (
+    <div className="bg-slate-400 p-5 mb-20">
+        <h1>{result}</h1>
+        <Button onClick={() => setResult(result+1)} children={"Increase"}/>
+    </div>
+
+  );
+};
+
 export default BtnTest;
--- components/pages/Test.tsx
+++ components/pages/Test.tsx
@@ -1,45 +1,10 @@
-import { MotionConfig } from "framer-motion";
-import Slidder from "../organisms/Slidder";
-import { motion } from "framer-motion";
-import React, { useRef, useState } from "react";
-import { useButton, FocusRing } from "react-aria";
+import BtnTest from "../atoms/Buttons/BtnTest";
 const Test = () => {
-  const [open, setOpen] = useState(false);
   return (
-    <div className="flex flex-col items-end justify-end bg-orange-100/20 h-[100vh] overflow-hidden items-center">
-      <motion.div
-        animate={{
-          opacity: open ? 0 : 1,
-        }}
-        transition={{
-          duration: 0.2,
-          ease: "easeInOut",
-        }}
-      >
-        <p className="max-w-[80vw] text-white">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et id
-          repellendus quia quasi vel quis veniam non! Ratione, molestiae ipsum?
-        </p>
-      </motion.div>
-        <Button onClick={() => setOpen(!open)} children={"Button"}/>
-      <Slidder />
+    <div className="h-[100vh] w-[100vw] flex items-end justify-center">
+      <BtnTest />
     </div>
   );
 };
 export default Test;
-interface ButtonProps {
-    onClick: () => void;
-    children: string;
-}
-function Button({onClick, children}:ButtonProps){
-    let ref = React.useRef() as React.RefObject<Element>;
-    let {buttonProps, isPressed} = useButton({onPress: onClick}, ref)
-    return(
-        <FocusRing focusRingClass="ring ring-offset-2 ring-offset-black">
-            <button {...buttonProps} className={`${isPressed ? "bg-blue-400/30" : "bg-blue-400"} px-4 py-2 rounded-md touch-none select-none focus:outline-none text-2xl`}>
-                {children}
-            </button>
-        </FocusRing>
-    )
-}
 
