686ad5edfecd12ee782d78b17a02c48c3fcc0763
Pablo <contactoemail.pablo@gmail.com>
2022-09-01 18:38:58 +0200

    Zustand Test with button 1.5

--- components/atoms/Buttons/BtnTest.tsx
+++ components/atoms/Buttons/BtnTest.tsx
@@ -1,17 +1,18 @@
-import { useAnimation } from "framer-motion";
-import React, { useRef, useState } from "react";
-import { useButton } from "react-aria";
+import React,{ useRef, ReactElement} from "react";
+import { AriaButtonProps, useButton, ButtonAria, } from "react-aria";
 import useAppStore from "../../Store";
-interface ButtonProps {
-  onClick: () => void;
+
+interface ButtonProps{
+  onClick: () => any;
   children: string | number;
 }
-
 function Button({ onClick, children }: ButtonProps) {
+  let ref = useRef();
+  let {buttonProps} = useButton( {newClick}, ref)
   return (
     <button
-      className="bg-blue-500/60 tracking-wide p-4 text-2xl rounded-lg select-none"
-      onClick={() => onClick()}
+      {...buttonProps}
+      className="bg-blue-500/60 tracking-wide p-4 text-2xl rounded-lg select-none touch-none"
     >
       {children}
     </button>
@@ -19,11 +20,11 @@ function Button({ onClick, children }: ButtonProps) {
 }
 
 const BtnTest = () => {
-  const data = useAppStore((state) => state.count)
-  const inc = useAppStore((state) => state.inc)
+  const count = useAppStore((state) => state.count);
+  const inc = useAppStore((state) => state.inc);
   return (
     <div className="p-5 mb-20 text-white flex flex-col space-y-5">
-      <h1 className="text-center text-6xl tabular-nums">{data}</h1>
+      <h1 className="text-center text-6xl tabular-nums">{count}</h1>
       <Button onClick={inc} children={"Increase"} />
     </div>
   );
