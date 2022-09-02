fbe0c132f822cd2faeb0e6ae5b5b3695895a2dcc
Pablo <contactoemail.pablo@gmail.com>
2022-09-02 13:44:33 +0200

    UI 1.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="bg-black h-[100vh] flex">
       <Head>
-        <title>Create Next App</title>
+        <title>DragTheBox</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
