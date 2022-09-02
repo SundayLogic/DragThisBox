b740207f38e74939502792ebac9a63e55aa3d274
Pablo <contactoemail.pablo@gmail.com>
2022-09-02 12:28:00 +0200

    Zustand Test with button 1.12: Fixing motion types, props need to be inside of span

--- components/atoms/Buttons/BtnTest.tsx
+++ components/atoms/Buttons/BtnTest.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { FocusRing, useButton } from "react-aria";
 import useAppStore from "../../Store";
-
 const BtnTest = () => {
   const count = useAppStore((state) => state.count);
   const inc = useAppStore((state) => state.inc);
@@ -11,8 +10,8 @@ const BtnTest = () => {
     <div className="p-5 mb-20 text-white flex flex-col space-y-5">
       <h1 className="text-center text-6xl tabular-nums">{count}</h1>
       <div className="flex space-x-4">
-        <Button onClick={dec} children={"-"} />
-        <Button onClick={inc} children={"+"} />
+        <Button children={"-"} />
+        <Button children={"+"} />
       </div>
     </div>
   );
@@ -21,28 +20,25 @@ const BtnTest = () => {
 export default BtnTest;
 
 interface ButtonProps {
-  onClick: () => any;
   children: string | number;
 }
-function Button({ onClick, children }: ButtonProps) {
+function Button({ children }: ButtonProps) {
+  const darkColor = "rgb(59 130 246 / 0.6)";
+  const lightColor = "rgb(147 197 253 / 0.6)";
   let controls = useAnimation();
   let ref = React.useRef() as React.RefObject<Element>;
-  const { buttonProps, isPressed } = useButton(
+  let { buttonProps, isPressed } = useButton(
     {
       onPressStart: () => {
         controls.stop();
         controls.set({
-          background: ["rgb(96 165 250 / 0.6)", "rgb(59 130 246 / 0.6)"],
+          background: lightColor,
         });
       },
       onPressEnd: () => {
-        controls.start({ background: "rgb(59 130 246 / 0.6)" });
-      },
-      onPress: () => {
-        onClick();
         controls.start({
-          background: ["rgb(96 165 250 / 0.6)", "rgb(59 130 246 / 0.6)"],
-          transition: { duration: 0.4 },
+          background: darkColor,
+          transition: { duration: 0.39 },
         });
       },
     },
@@ -50,13 +46,16 @@ function Button({ onClick, children }: ButtonProps) {
   );
   return (
     <FocusRing focusRingClass="ring ring-offset-2 ring-offset-black">
-      <motion.button animate={controls}>
-        <button
-          {...buttonProps}
-          className="bg-blue-500/60 tracking-wide px-8 py-4 text-2xl rounded-lg select-none touch-none focus:outline-none"
-        >
+      <motion.button
+        animate={controls}
+        style={{
+          WebkitTapHighlightColor: "transparent",
+        }}
+        className="bg-blue-500/60 tracking-wide px-8 py-4 cursor-pointer text-2xl rounded-lg select-none touch-none focus:outline-none"
+      >
+        <span {...buttonProps}>
           <h2 className="font-bold">{children}</h2>
-        </button>
+        </span>
       </motion.button>
     </FocusRing>
   );
