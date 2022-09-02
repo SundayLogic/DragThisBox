fb44a2d6f24cfbc584f209db98f5aeccdc063195
Pablo <contactoemail.pablo@gmail.com>
2022-09-02 11:42:18 +0200

    Zustand Test with button 1.11 : Button inside modtion.button

--- components/atoms/Buttons/BtnTest.tsx
+++ components/atoms/Buttons/BtnTest.tsx
@@ -3,46 +3,6 @@ import { motion, useAnimation } from "framer-motion";
 import { FocusRing, useButton } from "react-aria";
 import useAppStore from "../../Store";
 
-interface ButtonProps {
-  onClick: () => any;
-  children: string | number;
-}
-function Button({ onClick, children }: ButtonProps) {
-  const [isPressed, setIsPressed] = useState(false);
-  let controls = useAnimation();
-  let ref = React.useRef() as React.RefObject<Element>;
-  let { buttonProps } = useButton(
-    {
-      onPressStart: () => {
-        controls.stop();
-        controls.set({background: "rgb(96 165 250 / 0.6)"})
-      },
-      onPress: () => {
-        onClick();
-        controls.start({
-          background: ["rgb(96 165 250 / 0.6)", "rgb(59 130 246 / 0.6)"],
-        });
-      },
-    },
-    ref
-  );
-  return (
-    <FocusRing focusRingClass="ring ring-offset-2 ring-offset-black">
-      <motion.button
-        {...buttonProps}
-        className="bg-blue-500/60 tracking-wide px-8 py-4 text-2xl rounded-lg select-none touch-none focus:outline-none"
-        animate={controls}
-      >
-        <motion.h2
-          className="font-bold"
-        >
-          {children}
-        </motion.h2>
-      </motion.button>
-    </FocusRing>
-  );
-}
-
 const BtnTest = () => {
   const count = useAppStore((state) => state.count);
   const inc = useAppStore((state) => state.inc);
@@ -59,3 +19,45 @@ const BtnTest = () => {
 };
 
 export default BtnTest;
+
+interface ButtonProps {
+  onClick: () => any;
+  children: string | number;
+}
+function Button({ onClick, children }: ButtonProps) {
+  let controls = useAnimation();
+  let ref = React.useRef() as React.RefObject<Element>;
+  const { buttonProps, isPressed } = useButton(
+    {
+      onPressStart: () => {
+        controls.stop();
+        controls.set({
+          background: ["rgb(96 165 250 / 0.6)", "rgb(59 130 246 / 0.6)"],
+        });
+      },
+      onPressEnd: () => {
+        controls.start({ background: "rgb(59 130 246 / 0.6)" });
+      },
+      onPress: () => {
+        onClick();
+        controls.start({
+          background: ["rgb(96 165 250 / 0.6)", "rgb(59 130 246 / 0.6)"],
+          transition: { duration: 0.4 },
+        });
+      },
+    },
+    ref
+  );
+  return (
+    <FocusRing focusRingClass="ring ring-offset-2 ring-offset-black">
+      <motion.button animate={controls}>
+        <button
+          {...buttonProps}
+          className="bg-blue-500/60 tracking-wide px-8 py-4 text-2xl rounded-lg select-none touch-none focus:outline-none"
+        >
+          <h2 className="font-bold">{children}</h2>
+        </button>
+      </motion.button>
+    </FocusRing>
+  );
+}
